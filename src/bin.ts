7839a32887bf3f9337a91f3c20f1efd095952e1a
Anton Ödman <anton.odman@gmail.com>
2023-03-24 23:32:57 +0100

    chore: move to own bin folder

---
+++ src/bin.ts
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+import { sortBy } from "@banjoanton/utils";
+import { camelCase } from "change-case";
+import { Args, argv } from "./cli";
+import { loadConfig } from "./config";
+import "./features";
+import { setDebug } from "./runtime";
+import { CallService } from "./services/call-service";
+import { CustomService } from "./services/custom-service";
+import { GitService } from "./services/git-service";
+import { LogService } from "./services/log-service";
+import { standout } from "./utils";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const main = async (args: Args) => {
+    const shouldInit = args.flags.init;
+    const config = await loadConfig();
+
+    if (!config && !shouldInit) {
+        LogService.warning("No config found");
+        return;
+    }
+
+    if (config?.debug || args.flags.debug) setDebug();
+
+    if (shouldInit) {
+        await GitService.init(config);
+        return;
+    }
+
+    LogService.debug("Config found");
+
+    const hook = args.flags.type;
+    if (!CallService.isValidHook(hook)) {
+        LogService.debug("Invalid hook, exiting...");
+        return;
+    }
+
+    LogService.debug(`Hook type found: ${standout(hook)}`);
+
+    const camelCaseHook = camelCase(hook);
+    const hasCustomHook = CallService.hasCustomHook(camelCaseHook, config);
+    const features = CallService.getActiveFeatures(hook, config);
+
+    if (!features && !hasCustomHook) {
+        LogService.debug("No features or custom hook found, exiting...");
+        return;
+    }
+
+    LogService.info(`hoks > ${hook}`);
+
+    if (features) {
+        const sortedFeatures = sortBy(features, "priority", "desc");
+        LogService.debug("Running features...");
+        await CallService.runFeatures(sortedFeatures, args._, config);
+    }
+
+    if (hasCustomHook) {
+        LogService.debug("Running custom hook...");
+        await CustomService.runHook(camelCaseHook, args._, config);
+    }
+};
+
+main(argv);
