a5a22266dc9e0b1810b696dd030446ef91156cc7
Anton Ödman <anton.odman@gmail.com>
2023-03-29 13:07:26 +0200

    fix: use fs correctly

--- src/services/git-service.ts
+++ src/services/git-service.ts
@@ -57,13 +57,12 @@ const initializeHooks = async (config: FullConfig) => {
 
     LogService.debug("Updated git hooks path");
 
-    try {
-        await fs.rm(".git/hooks/");
-    } catch {}
-
-    if (!(await FileUtil.pathExists(".git/hooks/"))) {
-        await fs.mkdir(".git/hooks/");
+    const hooksFolder = ".git/hooks/";
+    const hooksFolderExists = await FileUtil.pathExists(hooksFolder);
+    if (hooksFolderExists) {
+        await fs.rm(hooksFolder, { recursive: true });
     }
+    await fs.mkdir(hooksFolder, { recursive: true });
 
     LogService.debug("Updated hooks directory");
 
